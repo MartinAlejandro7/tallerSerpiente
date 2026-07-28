7bad1082bddda770b865b253d516265be37b9586
MartinAlejandro7 <alejomartin7532@gmail.com>
2026-07-28 12:36:40 -0500

    se dibujo las lineas horizontales y verticales

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,47 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25
 
 
-    
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+// Primera pintura del juego al cargar la página
+dibujarTodo();
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+function dibujarTablero() {
+  ctx.strokeStyle = ("#d62323")
+ 
+
+  for(let x=0; x<canvas.width; x= x + TAMANIO_CELDA){
+    ctx.beginPath()
+    ctx.moveTo(x, 0)
+    ctx.lineTo(x,canvas.height)
+    ctx.stroke();
+  }
+
+  for(let y=0; y<canvas.height; y = y +TAMANIO_CELDA){
+   
+    ctx.beginPath()
+    ctx.moveTo(0, y)
+    ctx.lineTo(canvas.width, y)
+    ctx.stroke();
+  }
+}
+// =========================
+// FUNCIONES DE DIBUJO
+// =========================
+
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
 
 
 
